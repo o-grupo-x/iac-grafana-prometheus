4147171ebf15039c88df2f819fb8af682a19d7d1
Saler C. Lourenço <salerclourenco@gmail.com>
2025-06-05 21:40:13 -0300

    configura terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  default = "app-chamada-5706"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "instance_name" {
+  default = "instance-grafana-prometheus"
+}
